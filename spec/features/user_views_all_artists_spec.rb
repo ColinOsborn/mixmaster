69831e42575363f64b0278c872003d429f03506c
Colin Osborn <colin.osborn@gmail.com>
2016-06-29 21:10:24 -0600

    more artists
    adding more functionality and tests

--- spec/features/user_views_all_artists_spec.rb
+++ spec/features/user_views_all_artists_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "user views all artists spec" do
   scenario "viewing a link to individual artists page" do
-    
+    artist_name = "Cult of Luna"
+    artist_image = "http://totallystockholm.se/wp-content/uploads/2014/04/cult-of-luna.jpg"
+
+    Artist.create(name: artist_name, image_path: artist_image)
+
+    visit artists_path
+    expect(page).to have_content artist_name
+    click_on artist_name
+    expect(page).to have_content artist_name
+    expect(page).to have_css("img[src=\"#{artist_image}\"]")
   end
 end
